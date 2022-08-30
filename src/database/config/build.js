0093f823376ffc48c72b014697624df82e32c1eb
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-08-30 14:13:02 +0300

    Created the build.js and connection.js files Relates #6

---
+++ src/database/config/build.js
@@ -0,0 +1,11 @@
+const connection = require("./connection");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+
+const build = () => {
+  const sql = readFileSync(join(__dirname, "build.sql")).toString();
+
+  return connection.query(sql);
+};
+
+module.exports = build;
