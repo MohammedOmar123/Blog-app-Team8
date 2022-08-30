9fc4311bc11e7586aaac8ec5dacd39c19f210519
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-08-30 14:02:07 +0300

    add url image in seeder file relates #3

--- src/database/config/seeder.sql
+++ src/database/config/seeder.sql
@@ -1,13 +1,13 @@
 BEGIN;
 
 insert into users(username, email, password, image) values 
-('Mustafa', 'mustafa@gmail.com', '123456', 'url'), 
-('Zayan', 'zayan@gmail.com', '123456789', 'zayanUrl');
+('Mustafa', 'mustafa@gmail.com', '123456', 'https://images.pexels.com/photos/13278016/pexels-photo-13278016.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'), 
+('Zayan', 'zayan@gmail.com', '123456789', 'https://images.pexels.com/photos/13278016/pexels-photo-13278016.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
 
 insert into posts(title, content, image, userId) values 
-('IMPORTANt', 'this is too important!', 'imURL', 1), 
-('Hello', 'Welcome', 'HiURL', 2),
-('test', 'test content ', 'testteset', 1);
+('IMPORTANt', 'this is too important!', 'https://images.pexels.com/photos/13278016/pexels-photo-13278016.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1), 
+('Hello', 'Welcome', 'https://images.pexels.com/photos/13278016/pexels-photo-13278016.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 2),
+('test', 'test content ', 'https://images.pexels.com/photos/13278016/pexels-photo-13278016.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 1);
 
 
 
