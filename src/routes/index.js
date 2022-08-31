9f278cd471dee20ea36dfae4907db509ff4fe84f
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-01 01:26:13 +0300

    Created logout route Relates #56

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,13 @@ const login = require('./login');
 const addPost = require('./addPost');
 const getUserPosts = require('./getUserPosts');
 const getPosts = require('./getPosts');
+const logout = require('./logout');
 
 router.use(addPost);
-
 router.use(signup);
-
 router.use(getPosts);
-
 router.use(login);
 router.use(getUserPosts);
+router.use(logout);
 
 module.exports = router;
