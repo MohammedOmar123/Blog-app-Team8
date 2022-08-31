fd675af8df60c60fabf9bb64220537fbddece19c
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-31 19:20:06 +0300

    refactor verify token middleware

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
   const { token } = req.cookies;
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-    if (err) { res.status(300).redirect('/signup'); } else {
+    if (err) { res.status(300).json({ massage: 'signup.html' }); } else {
       req.token = decoded;
       next();
     }
