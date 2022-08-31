2c310b6c8acac06cf4e20b5265ba9932ac8ae164
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-08-31 22:03:52 +0300

    change the secret key name from SECRET_KEY to secret

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
   console.log('token');
   const { token } = req.cookies;
-  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+  jwt.verify(token, process.env.secret, (err, decoded) => {
     if (err) { res.status(300).json({ massage: 'signup.html' }); } else {
       req.token = decoded;
       next();
