f5a01f7a5213bd396443947f038bff6f732e5858
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-01 00:00:32 +0300

    Created delete post controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,6 @@
 const { postUser, login } = require('./users');
-const { createPost, getUserPosts, getPosts } = require('./posts');
+const { createPost, getUserPosts, getPosts, deletePost} = require('./posts');
 
 module.exports = {
-  postUser, login, createPost, getPosts, getUserPosts,
+  postUser, login, createPost, getPosts, getUserPosts, deletePost
 };
